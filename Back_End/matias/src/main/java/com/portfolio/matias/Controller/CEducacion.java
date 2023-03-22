eae2519c2f03a52335d2ca2650c3b1daea9e8f14
MatiSantachita <matiassantachita@gmail.com>
2023-03-22 12:24:16 -0300

    mod_link_firebase
    modificando el localhost por el link proporcionado por firebase

--- Back_End/matias/src/main/java/com/portfolio/matias/Controller/CEducacion.java
+++ Back_End/matias/src/main/java/com/portfolio/matias/Controller/CEducacion.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://portfolio-matias.web.app"})
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
